a1043b74199cec00ebe19627c14e06d5225c1071
Alti1302 <altieres.silva@hotmail.com>
2024-10-13 22:44:21 -0300

    Exemplo Operador Ternário

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+    int nota=10;
+
+    String resultado = nota >=6 ? "Aprovado!" : "Reprovado!";
+        System.out.println("O resultado final após avaliação é:" + resultado);
+    }
+}
